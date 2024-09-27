82700c22962e770f444f23d51f59e48b0a88db72
supertorpe <supertorpe@hotmail.com>
2024-09-27 12:34:28 +0200

    close menu properly

--- dev-dist/registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/ionic-alpinejs-navigo/dev-sw.js?dev-sw', { scope: '/ionic-alpinejs-navigo/', type: 'classic' })
--- src/commons/utils.ts
+++ src/commons/utils.ts
@@ -1,9 +1,28 @@
 import Navigo from 'navigo';
+import { MenuI, menuController } from '@ionic/core';
 import { Controller } from './controller';
 
 const templates = new Map<string, string>();
 
+let menuRegistered = false;
+const closeMenu = (menuId: string) => {
+    if (!menuRegistered) {
+        const query = "ion-menu[menu-id='" + menuId + "']";
+        const menu = document.querySelector(query) as unknown as MenuI;
+        if (!menu) return;
+        menuController._register(menu);
+        menuRegistered = true;
+    }
+    menuController.close(menuId);
+}
+
+const loadContent = (targetElement: Element, content: string) => {
+    // TO DO: add some transition effect
+    targetElement.innerHTML = content;
+};
+
 export const openRoute = (navigo: Navigo, template: string, targetElementSelector: string, controller: Controller, params?: any): Promise<void> => {
+    closeMenu('main-menu');
     return new Promise<void>(resolve => {
         const targetElement = document.querySelector(targetElementSelector);
         if (targetElement) {
@@ -21,13 +40,13 @@ export const openRoute = (navigo: Navigo, template: string, targetElementSelecto
             controller.preload(params);
             const content = templates.get(template);
             if (content) {
-                targetElement.innerHTML = content;
+                loadContent(targetElement, content);
             } else {
                 fetch(template)
                 .then(response => response.text()
                     .then(content => {
                         templates.set(template, content);
-                        targetElement.innerHTML = content;
+                        loadContent(targetElement, content);
                     }));
             }
         }
